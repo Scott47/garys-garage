ab0eb69a030d23b10d1b6448749eb8e735ef7a6a
scottsilver <scottsilver47@gmail.com>
2021-11-26 10:27:04 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace garysgarage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
